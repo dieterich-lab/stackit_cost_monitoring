1bf17b48410d7d5f73c03e4d284a13474427ef46
Harald Wilhelmi <harald.wilhelmi@uni-heidelberg.de>
2026-01-19 10:49:39 +0100

    Improved handling of zero cost case

--- src/stackit_cost_monitoring/nagios_plugin.py
+++ src/stackit_cost_monitoring/nagios_plugin.py
@@ -46,10 +46,10 @@ class NagiosReporter:
 
     def book_cost_item(self, cost_item: CostApiItem):
         if cost_item.reportData is None:
-            self._report_data_message = 'CostApi returned no reportData'
+            self._book_total_cost(cost_item, 'CostApi returned no reportData')
             return
         if len(cost_item.reportData) == 0:
-            self._report_data_message = 'CostApi returned empty reportData'
+            self.__book_total_cost(cost_item, 'CostApi returned empty reportData')
             return
         for report_data in cost_item.reportData:
             if self._report_date is not None and report_data.timePeriod.start < self._report_date:
@@ -57,6 +57,11 @@ class NagiosReporter:
             self._report_date = report_data.timePeriod.start
             self._cost = report_data.charge / CENTS_PER_EURO
             self._discounted_cost = report_data.discount / CENTS_PER_EURO
+
+    def _book_total_cost(self, cost_item: CostApiItem, message: str):
+        self._report_data_message = message
+        self._cost = cost_item.totalCharge / CENTS_PER_EURO
+        self._discounted_cost = cost_item.totalDiscount / CENTS_PER_EURO
 
     def do_report(self) -> NoReturn:
         cost = self._cost
@@ -82,17 +87,25 @@ class NagiosReporter:
 
         if not self.args.skip_discounts:
             cost += self._discounted_cost
-        report_date_str = self._report_date.strftime('%Y-%m-%d')
+        if self._report_date is None:
+            report_date_str = '(unknown date - no detailed report data)'
+        else:
+            data_str = self._report_date.strftime('%Y-%m-%d')
+            report_date_str = f"for {data_str}"
 
         if cost >= self.args.critical:
             exit_code = NagiosExitCodes.CRITICAL
-            message = f"Daily costs {cost:.2f} EUR >= {self.args.critical} EUR for {report_date_str}"
+            message = f"Daily costs {cost:.2f} EUR >= {self.args.critical} EUR {report_date_str}"
         elif cost >= self.args.warning:
             exit_code = NagiosExitCodes.WARNING
-            message = f"Daily costs {cost:.2f} EUR >= {self.args.warning} EUR for {report_date_str}"
+            message = f"Daily costs {cost:.2f} EUR >= {self.args.warning} EUR {report_date_str}"
+        elif cost > 0 and self._report_data_message is not None:
+            exit_code = NagiosExitCodes.WARNING
+            message = f"No detailed reportData ({self._report_data_message}) " \
+                    f"but non-zero costs {cost:.2f} EUR"
         else:
             exit_code = NagiosExitCodes.OK
-            message = f"Daily costs {cost:.2f} EUR for {report_date_str}"
+            message = f"Daily costs {cost:.2f} EUR {report_date_str}"
 
         return self._finish(exit_code, message)
 
